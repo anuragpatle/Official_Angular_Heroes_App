519c324cb1b8c240bcb040e86e5a874fb11f61e5
anuragpatle <anurag.ap6@gmail.com>
2020-02-27 18:02:28 +0530

    Using Operators with Observables
    closes #7

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, Operator } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -26,10 +27,22 @@ export class DashboardComponent implements OnInit, OnDestroy {
       setInterval(() => {
         observer.next(count);
         count++;
-      }, 1000);
+      }, 500);
     });
 
-    this.firstObsSubscription = customIntervalObserver.subscribe(data => {
+
+    // let modifedOperator: Observable<number>
+    const modifedObservable = customIntervalObserver.pipe(filter((data: number) =>{
+      return data > 0;
+    }), map((data: number) => {
+
+      // If counter is running in 500 ms then to get the output
+      // in seconds properly we have to divide the data by 2.
+      // Half a counting per increment [per increment means every 500 ms second our count values becomes .5 second]
+      return data / 2;
+    }));
+
+    this.firstObsSubscription = modifedObservable.subscribe(data => {
       this.timeOfDashboardComponentInit = data as number;
     });
   }
